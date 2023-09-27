1da669587d6a6b892989217891fb26eece520f05
Huawei <Huawei@LAPTOP-Q85GDL68>
2023-09-27 18:44:38 +0300

    Добавьте файлы проекта.

---
+++ ДЗ 3/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ДЗ_3
+{
+    enum weekday
+    {
+        Понедельник = 1,
+        Вторник,
+        Среда,
+        Четверг,
+        Пятница,
+        Суббота,
+        Воскресенье
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Номер 1 \n Программа определяет, является ли последовательность упорядочненной по возрастанию");
+            int[] sequence = new int[10];
+            bool ordered = true;
+            int index = -1;
+
+            Console.WriteLine("Введите последовательность из 10 чисел:");
+
+            for (int i = 0; i < sequence.Length; i++)
+            {
+                Console.WriteLine("Число {0}: ", i + 1);
+                sequence[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < sequence.Length - 1; i++)
+            {
+                if (sequence[i] > sequence[i + 1])
+                {
+                    ordered = false;
+                    index = i + 2;
+                    break;
+                }
+            }
+
+            if (ordered)
+            {
+                Console.WriteLine("Последовательность упорядочена по возрастанию.");
+            }
+            else
+            {
+                Console.WriteLine("Последовательность не упорядочена по возрастанию. Нарушение на позиции " + index);
+            }
+
+
+            Console.WriteLine("Номер 2 \n Программа определяет достоинство соответствующей карты");
+            int n;
+            try
+            {
+                Console.WriteLine("Введите номер карты от 6 до 14");
+                n = int.Parse(Console.ReadLine());
+
+                string rank;
+                if (n >= 6 && n <= 10)
+                {
+                    rank = n.ToString();
+                }
+                else if (n == 11)
+                {
+                    rank = "валет";
+                }
+                else if (n == 12)
+                {
+                    rank = "дама";
+                }
+                else if (n == 13)
+                {
+                    rank = "король";
+                }
+                else if (n == 14)
+                {
+                    rank = "туз";
+                }
+                else
+                {
+                    throw new Exception("Нужно было ввести номер карты от 6 до 14");
+                }
+                Console.WriteLine($"Достоинство карты номера {n} = {rank}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Ошибка: {e.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Программа завершена.");
+            }
+            Console.WriteLine("Номер 3 \n Программа выводит значения в соответствии с таблицей");
+            Console.WriteLine("Введите jabroni/school counselor/programmer/bike gang member/politician/rapper/");
+            string a = Console.ReadLine();
+            a = a.ToLower();
+            switch (a)
+            {
+                case "jabroni":
+                    Console.WriteLine("Patron Tequila");
+                    break;
+                case "school counselor":
+                    Console.WriteLine("Anything with Alcohol");
+                    break;
+                case "programmer":
+                    Console.WriteLine("Hipster Craft Beer");
+                    break;
+                case "bike gang member":
+                    Console.WriteLine("Moonshine");
+                    break;
+                case "politician":
+                    Console.WriteLine("Your tax dollars");
+                    break;
+                case "rapper":
+                    Console.WriteLine("Cristal"); 
+                    break;
+                default: Console.WriteLine("Beer"); 
+                    break;
+
+            }
+
+            Console.WriteLine("Номер 4 \n Программа выводит названия дня недели в зависимости от введённого порядкового номера \n Введите порядковый номер дня недели от 1 до 7");
+            int number = int.Parse(Console.ReadLine());
+            if (number >=1 && number <=7)
+            {
+                weekday day = (weekday)number;
+                Console.WriteLine(day);
+            }
+            else
+            {
+                Console.WriteLine("Нужно было ввести порядковый номер дня недели от 1 до 7");
+            }
+
+            Console.WriteLine("Номер 5 \n Программа обходит весь массив и при встречи элементов HelloKitty и Barbie doll считает их количество");
+            string[] toys = { "Hello Kitty", "Barbie doll", "Teddy", "Hello Kitty" , "LOL", "Furby"};
+
+            int quantity = 0;
+            foreach (string dolls in toys)
+            {
+                if (dolls == "Hello Kitty" || dolls == "Barbie doll")
+                {
+                    quantity++;
+                }
+            }
+
+            Console.WriteLine($"Количество кукол в сумке = {quantity}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ тумаков 4/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace тумаков_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Упражнение 4.1,4.2 \n Программа читает с экрана номер дня в году и переводит этот число в месяц и день месяца");
+            Console.WriteLine("Введите номер дня в году");
+            int n = int.Parse(Console.ReadLine());
+            DateTime date = Convert.ToDateTime("01.01.1999");
+
+            if ((n>365) || (n<=0))
+            {
+                Console.WriteLine("Столько дней в году не бывает, нужно было ввести число от 1 до 365");
+            }
+            else
+            {
+                date = date.AddDays(n - 1);
+                Console.WriteLine(date.ToString("d MMMM"));
+            }
+
+            Console.WriteLine("Домашнее задание 4.1");
+            Console.WriteLine("Программа читает с экрана номер дня в году и переводит этот число в месяц и день месяца, а также определяет, високосный год или нет");
+            Console.WriteLine("Введите номер дня в году");
+
+            int x = int.Parse(Console.ReadLine());
+            DateTime date1 = Convert.ToDateTime("01.01.1999");
+            Console.WriteLine("Введите год");
+            int year = int.Parse(Console.ReadLine());
+
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+            {
+                Console.WriteLine("Год високосный");
+                if ((x > 366) || (x <= 0))
+                {
+                    Console.WriteLine("Столько дней в году не бывает, нужно было ввести число от 1 до 366");
+                }
+                else
+                {
+                    date1 = date1.AddDays(x - 1);
+                    Console.WriteLine(date1.ToString("d MMMM") + " " + year + " года");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Год невисокосный");
+                if ((x > 366) || (x <= 0))
+                {
+                    Console.WriteLine("Столько дней в году не бывает, нужно было ввести число от 1 до 366");
+                }
+                else
+                {
+                    date1 = date1.AddDays(x - 1);
+                    Console.WriteLine(date1.ToString("d MMMM") + " " + year + " года");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
